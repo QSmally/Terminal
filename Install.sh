9fdf66b4763757d3309b205ee7420556c1725fca
QSmally <github@qbot.eu>
2023-09-05 15:22:54 +0200

    Project: fix install script on empty systems

--- Install.sh
+++ Install.sh
@@ -8,6 +8,9 @@ declare -r files=(
 
 declare -i linked_file_count=0
 
+mkdir -p ~/.vim
+mkdir -p ~/.cache
+
 for path in "${files[@]}"; do
     filename=$(basename $path)
     directory=$(dirname $path)
@@ -18,7 +21,7 @@ for path in "${files[@]}"; do
     fi
 
     if [ ! -d $directory ]; then
-        mkdir $directory
+        mkdir -p $directory
         echo "Intermediate directory $directory created"
     fi
 
@@ -31,4 +34,3 @@ for path in "${files[@]}"; do
 done
 
 echo "Installed a total of $linked_file_count configuration files"
-
