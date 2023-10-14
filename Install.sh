cb56d1c3f7296a446cb9fd441d02b59a1e920d94
QSmally <github@qbot.eu>
2023-10-14 15:12:22 +0200

    Project: exhaustive installation script

--- Install.sh
+++ Install.sh
@@ -1,25 +1,15 @@
 #!/bin/bash
 
-declare -r files=(
-    ~/.bash_profile
-    ~/.gitconfig
-    ~/.gitignore
-    ~/.vim/colors/default-dark.vim
-    ~/.vimrc
-)
-
 declare -i linked_file_count=0
 
-mkdir -p ~/.vim
-mkdir -p ~/.cache
-
-for path in "${files[@]}"; do
+install() {
+    path="$1"
     filename=$(basename $path)
     directory=$(dirname $path)
 
     if [ -e $path ]; then
         echo "File $filename ignored because it already exists in $directory"
-        continue
+        return 1
     fi
 
     if [ ! -d $directory ]; then
@@ -27,12 +17,45 @@ for path in "${files[@]}"; do
         echo "Intermediate directory $directory created"
     fi
 
-    ln -s $(pwd)/$filename $directory
+    ln -s $(pwd)/$filename $directory/$2
 
     if [ $? -eq 0 ]; then
         linked_file_count+=1
         echo "Symlinked $filename to $directory"
     fi
-done
+}
+
+dependency() {
+    if [ ! $(which $2 2> /dev/null) ]; then
+        echo "Caveat: $1 may require executable $2, but it's not in PATH"
+    fi
+}
+
+mkdir -p ~/.cache
+
+install ~/.gitconfig
+install ~/.gitignore
+install ~/.gittemplate/hooks/commit-msg
+install ~/.vim/colors/default-dark.vim
+install ~/.vimrc
+
+dependency vim vim
+dependency git git
+
+if [ $(uname) == "Linux" ]; then
+    # Debian: default bashrc loads bash_aliases, use bash_local for locals
+    install ~/.bash_profile .bash_aliases
+    install ~/.xinitrc
+    dependency xorg startx
+    dependency xorg dwm
+else
+    install ~/.bash_profile
+fi
+
+dependency vim ack
+dependency vim curl
+dependency vim ctags
+dependency vim docker
+dependency vim xpdf
 
 echo "Installed a total of $linked_file_count configuration files"
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 # Terminal configuration
 
-Bash and Vim dotfiles.
+Bash, Git and Vim dotfiles.
 
 Symlink all relevant files to `~` or use the `Install.sh` script. For Vim, plugins are only enabled
 and configured when [Plug](https://github.com/junegunn/vim-plug) is installed to remove any issues
